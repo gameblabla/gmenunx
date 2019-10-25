ed495f1f5e1088787557c62ed9974f591a94ebce
podulator <github-account@podulator.com>
2019-10-25 20:54:57 +0200

    dump the scp command out

--- quick-rg.sh
+++ quick-rg.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 export CROSS_COMPILE=/opt/gcw0-toolchain/usr/bin/mipsel-linux- && \
-export DEBUG=4 && \
+export DEBUG=5 && \
 make -f ./Makefile.rg-350 clean && \
 make -f ./Makefile.rg-350 && \
+echo "scp ./objs/rg-350/gmenunx root@10.1.1.2:/media/data/local/home" && \
 scp ./objs/rg-350/gmenunx root@10.1.1.2:/media/data/local/home && \
 echo "
 ssh to :: root@10.1.1.2
