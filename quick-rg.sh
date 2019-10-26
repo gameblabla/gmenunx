122b4b3a9a520b900cb5230a46254de03fd72e8b
podulator <github-account@podulator.com>
2019-10-26 16:36:54 +0200

    better ignore file, better quick script

--- quick-rg.sh
+++ quick-rg.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
 export CROSS_COMPILE=/opt/gcw0-toolchain/usr/bin/mipsel-linux- && \
-#export DEBUG=5 && \
-#make -f ./Makefile.rg-350 clean && \
-#make -f ./Makefile.rg-350 && \
-make -f ./Makefile.rg-350 dist
+export DEBUG=5
+
+if [ $# -ne 1 ]; then
+make -f ./Makefile.rg-350 clean
+	if [ $? -ne 0 ]; then
+		echo "make -f ./Makefile.rg-350 clean failed"
+		exit 1
+	fi
+fi
+
+make -f ./Makefile.rg-350
+if [ $? -ne 0 ]; then
+	echo "make -f ./Makefile.rg-350 failed"
+	exit 1
+fi
+	
+if [ $# -ne 1 ]; then
+	make -f ./Makefile.rg-350 dist
+	if [ $? -ne 0 ]; then
+		echo "make -f ./Makefile.rg-350 dist failed"
+		exit 1
+	fi
+fi
 
 echo "
 /*                     *\\
@@ -21,13 +40,11 @@ while [[ $success -ne 0 && $tries -lt 10 ]];
 do
 	scp ./objs/rg-350/gmenunx root@10.1.1.2:/media/data/local/home
 	success=$?
-	#echo $success
 	tries=$[$tries+1]
-	#echo $tries
 done
 if [[ $success -ne 0 ]]; then
-	printf "Failed to copy the app over. \nEnter terminal mode on the rg350, or move your\n"
-	printf "/usr/local/sbin/frontend_start\n"
+	printf "Failed to copy the app over. \nEnter terminal mode on the rg350, or move your "
+	printf "/usr/local/sbin/frontend_start "
 	printf "file for a while\n"
 else
 	printf "ssh to :: root@10.1.1.2\nand run :: \n"
@@ -35,5 +52,3 @@ else
 	printf "or\n"
 	printf "strace -p\`pidof frontend_start\` -s99999 -e trace= -e write\n"
 fi
-
-
