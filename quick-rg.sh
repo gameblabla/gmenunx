c6f7c42b9dda113bda5e492cd934910c946a5685
podulator <github-account@podulator.com>
2019-10-28 07:00:38 +0100

    updated helper scripts, add about.txt to makefile

--- quick-rg.sh
+++ quick-rg.sh
@@ -1,29 +1,143 @@
 #!/bin/bash
 
-export CROSS_COMPILE=/opt/gcw0-toolchain/usr/bin/mipsel-linux- && \
-export DEBUG=5
-
-if [ $# -ne 1 ]; then
-make -f ./Makefile.rg-350 clean
-	if [ $? -ne 0 ]; then
-		echo "make -f ./Makefile.rg-350 clean failed"
-		exit 1
-	fi
-fi
-
-make -f ./Makefile.rg-350
+rsync=`which rsync`
 if [ $? -ne 0 ]; then
-	echo "make -f ./Makefile.rg-350 failed"
+	echo "This script requires rsync"
+	exit 1
+fi
+scp=`which scp`
+if [ $? -ne 0 ]; then
+	echo "This script requires scp"
 	exit 1
 fi
 
-if [ $# -ne 1 ]; then
-	make -f ./Makefile.rg-350 dist
-	if [ $? -ne 0 ]; then
-		echo "make -f ./Makefile.rg-350 dist failed"
+RG350_HOME="/media/data/local/home"
+LAUNCHER="/usr/local/sbin/frontend_start"
+RG350_IP="root@10.1.1.2"
+TARGET_DIR="gmenunx-beta"
+
+export CROSS_COMPILE=/opt/gcw0-toolchain/usr/bin/mipsel-linux- && \
+export DEBUG=5
+
+function launchLinkExists {
+	echo "checking if a launchLink exists"
+	cmd="ssh ${RG350_IP} /bin/busybox sh -c \"if [ -L ${LAUNCHER} ]; then echo \"EXISTS\"; fi\""
+	result=`${cmd}`
+	if [ "EXISTS" == ${result} ]; then
+		return 1
+	else
+		return 0
+	fi
+}
+
+function removeLaunchLink {
+	echo "removing launch link"
+	cmd="ssh ${RG350_IP} /bin/busybox sh -c \"if [ -L ${LAUNCHER} ]; then rm -rf ${LAUNCHER}; fi\""
+	${cmd}
+	if [ $? -eq 0 ]; then
+		echo "removed launcher link successfully"
+	else
+		echo "failed to remove launch link, quitting"
 		exit 1
 	fi
+}
+
+function installLaunchLink {
+	echo "installing launch link"
+	cmd="ssh ${RG350_IP} /bin/busybox sh -c \"ln -fs ${RG350_HOME}/${TARGET_DIR}/gmenunx ${LAUNCHER}\""
+	echo ${cmd}
+	${cmd}
+	if [ $? -eq 0 ]; then
+		echo "installed launcher link successfully"
+	else
+		echo "failed to install launch link, quitting"
+		exit 1
+	fi
+}
+
+function fullDeploy {
+	echo "doing a full deploy from target folder to rg-350"
+	cmd="rsync -r ./dist/rg-350/gmenunx/ ${RG350_IP}:${RG350_HOME}/${TARGET_DIR}"
+	echo ${cmd}
+	${cmd}
+	if [ $? -ne 0 ]; then
+		echo "full deployment failed"
+		exit 1
+	fi
+	return $?
+}
+
+function binaryDeploy {
+	echo "doing a quick binary only deploy"
+	cmd="scp ./objs/rg-350/gmenunx ${RG350_IP}:${RG350_HOME}/${TARGET_DIR}"
+	echo ${cmd}
+	${cmd}
+	return $?
+}
+
+function showHelp {
+      echo "Usage:"
+      echo "    -h                 Display this help message."
+      echo "    -f                 Full clean, build, deploy, enable."
+	  echo "    -q                 Quick, build, deploy binary only."
+	  echo "    -i                 install"
+	  echo "    -u                 uninstall"
+}
+
+function myMake {
+	args="$*"
+	cmd="make -f ./Makefile.rg-350 ${args}"
+	echo "make command : ${cmd}"
+	${cmd}
+	if [ $? -ne 0 ]; then
+		echo "make failed"
+		exit 1
+	fi
+}
+
+if [ $# -eq 0 ]; then
+	showHelp
+	exit 0
 fi
+
+while getopts ":hfqiu" opt; do
+
+  case ${opt} in
+	h )
+		showHelp
+		exit 0
+      ;;
+    f )
+		myMake "clean all dist"
+		fullDeploy
+		installLaunchLink
+		exit 0
+      ;;
+    q )
+		myMake "all"
+		binaryDeploy
+		exit 0
+      ;;
+    i )
+		installLaunchLink
+		exit 0
+      ;;
+    u )
+		removeLaunchLink
+		exit 0
+      ;;
+    \? )
+      showHelp
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+	  showHelp
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+exit 0
+
 
 echo "
 /*                     *\\
