ca402a40358fb08d18edaf0b8a0bc4002b137cbf
podulator <github-account@podulator.com>
2019-10-28 06:59:51 +0100

    update opk script

--- make_opk.sh
+++ make_opk.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 OPK_NAME=gmenunx.opk
+ASSETS_PATH="dist/rg-350/gmenunx"
 
+if [ ! -d ${ASSETS_PATH} ]; then
+    echo "Can't fid ${ASSETS_PATH}, You need to run a make dist first"
+    exit 1
+fi
 echo ${OPK_NAME}
 
 # create default.gcw0.desktop
@@ -9,22 +14,18 @@ cat > default.gcw0.desktop <<EOF
 [Desktop Entry]
 Name=GmenuNX
 Comment=GmenuNX
-Exec=gmenu2x
+Exec=gmenunx
 Terminal=false
 Type=Application
 StartupNotify=true
-Icon=generic
+Icon=rg350
 Categories=applications;
 EOF
 
 # create opk
-FLIST="objs/rg-350/gmenunx"
+FLIST="${ASSETS_PATH}/*"
 FLIST="${FLIST} default.gcw0.desktop"
-FLIST="${FLIST} assets/skins/Default/icons/generic.png"
-FLIST="${FLIST} assets/skins"
-FLIST="${FLIST} assets/translations"
-FLIST="${FLIST} assets/rg-350/*"
-FLIST="${FLIST} about.txt"
+FLIST="${FLIST} assets/rg-350/icons/rg350.png"
 
 rm -f ${OPK_NAME}
 mksquashfs ${FLIST} ${OPK_NAME} -all-root -no-xattrs -noappend -no-exports
