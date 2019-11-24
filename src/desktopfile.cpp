dc07e885a3d4770b3edabfe329687e220cbd2c17
podulator <github-account@podulator.com>
2019-11-24 14:25:36 +0100

    basic implementation of a desktop file

---
+++ src/desktopfile.cpp
@@ -0,0 +1,152 @@
+#include "debug.h"
+#include "desktopfile.h"
+#include "utilities.h"
+#include <vector>
+#include <unistd.h>
+#include <fstream>
+#include <sstream>
+
+#define sync() sync(); system("sync");
+
+using std::ifstream;
+using std::ofstream;
+using std::string;
+
+DesktopFile::DesktopFile() {
+    this->reset();
+}
+
+DesktopFile::DesktopFile(const string & file) {
+    this->reset();
+    this->fromFile(file);
+}
+
+DesktopFile::~DesktopFile() {
+    //TRACE("DesktopFile::~DesktopFile - enter");
+}
+
+string DesktopFile::toString() {
+
+    vector<string> vec;
+
+    vec.push_back("# gmenunx desktop file");
+    vec.push_back("# lines starting with a # are ignored");
+
+    vec.push_back(string_format("title=\"%s\"", this->title().c_str()));
+    vec.push_back(string_format("description=\"%s\"", this->description().c_str()));
+    vec.push_back(string_format("icon=\"%s\"", this->icon().c_str()));
+    vec.push_back(string_format("exec=\"%s\"", this->exec().c_str()));
+    vec.push_back(string_format("params=\"%s\"", this->params().c_str()));
+    vec.push_back(string_format("selectordir=\"%s\"", this->selectordir().c_str()));
+    vec.push_back(string_format("selectorfilter=\"%s\"", this->selectorfilter().c_str()));
+    vec.push_back(string_format("x-provider=\"%s\"", this->provider().c_str()));
+    vec.push_back(string_format("consoleapp=\"%s\"", this->consoleapp() ? "true" : "false"));
+
+    std::string s;
+    for (const auto &piece : vec) s += (piece + "\n");
+    return s;
+
+}
+
+bool DesktopFile::fromFile(const string & file) {
+    TRACE("DesktopFile::fromFile - enter : %s", file.c_str());
+    bool success = false;
+    if (fileExists(file)) {
+        TRACE("DesktopFile::fromFile - found desktop file");
+        this->path_ = file;
+
+		std::ifstream confstream(this->path_.c_str(), std::ios_base::in);
+		if (confstream.is_open()) {
+			string line;
+			while (getline(confstream, line, '\n')) {
+				line = trim(line);
+                if (0 == line.length()) continue;
+                if ('#' == line[0]) continue;
+				string::size_type pos = line.find("=");
+                if (string::npos == pos) continue;
+                
+				string name = trim(line.substr(0,pos));
+				string value = trim(line.substr(pos+1,line.length()));
+
+                if (0 == value.length()) continue;
+                name = toLower(name);
+                //TRACE("Config::fromFile - handling kvp - %s = %s", name.c_str(), value.c_str());
+
+                if (name == "title") {
+                    this->title(stripQuotes(value));
+                } else if (name == "description") {
+                    this->description(stripQuotes(value));
+                } else if (name == "icon") {
+                    this->icon(stripQuotes(value));
+                } else if (name == "exec") {
+                    this->exec(stripQuotes(value));
+                } else if (name == "params") {
+                    this->params(stripQuotes(value));
+                } else if (name == "selectordir") {
+                    this->selectordir(stripQuotes(value));
+                } else if (name == "selectorfilter") {
+                    this->selectorfilter(stripQuotes(value));
+                } else if (name == "x-provider") {
+                    this->provider(stripQuotes(value));
+                } else if (name == "consoleapp") {
+                    bool console = "false" == stripQuotes(value) ? false : true;
+                    this->consoleapp(console);
+                } else {
+                    WARNING("DesktopFile::fromFile - unknown key : %s", name.c_str());
+                }
+            };
+            confstream.close();
+            success = true;
+            this->isDirty_ = false;
+        }
+    } else {
+        TRACE("DesktopFile::fromFile - desktopfile doesn't exist : %s", file.c_str());
+    }
+    TRACE("DesktopFile::fromFile - exit - success: %i", success);
+    return success;
+}
+
+void DesktopFile::remove() {
+    TRACE("DesktopFile::remove - enter");
+    if (!this->path_.empty()) {
+        TRACE("DesktopFile::remove - deleting file : %s", this->path_.c_str());
+        unlink(this->path_.c_str());
+    }
+    TRACE("DesktopFile::remove - exit");
+}
+
+bool DesktopFile::save(const string & path) {
+    TRACE("DesktopFile::save - enter : %s", path.c_str());
+    if (this->isDirty_) {
+        if (!path.empty()) {
+            if (fileExists(path)) {
+                unlink(path.c_str());
+            }
+            this->path_ = path;
+        }
+        TRACE("Config::save - saving to : %s", this->path_.c_str());
+        std::ofstream desktop(this->path_.c_str());
+        if (desktop.is_open()) {
+            desktop << this->toString();
+            desktop.close();
+            sync();
+            this->isDirty_ = false;
+        }
+    }
+    TRACE("Config::save - exit");
+    return true;
+}
+
+void DesktopFile::reset() {
+    this->path_ = "";
+    this->title_ = "";
+    this->description_ = "";
+    this->icon_ = "";
+    this->exec_ = "";
+    this->params_ = "";
+    this->selectordir_ = "";
+    this->selectorfilter_ = "";
+    this->provider = "";
+    bool consoleapp_ = false;
+    this->isDirty_ = false;
+}
---
+++ src/desktopfile.h
@@ -0,0 +1,105 @@
+#ifndef DESKTOPFILE_H_
+#define DESKTOPFILE_H_
+
+#include <string>
+
+using namespace std;
+
+class DesktopFile {
+    private:
+
+        void reset();
+
+        bool isDirty_;
+        string path_; 
+
+        string title_; //=ReGBA
+        string description_; //=Game Boy Advance Emulator
+        string icon_; //=/home/mat/.gmenunx/skins/Default/icons/regba-silver.png
+        string exec_; //=/bin/bash
+        string params_;  //=-m default.gcw0.desktop /home/mat/Downloads/games/rg\-350/regba_fast\ \(1\).opk 
+        string selectordir_; //=/home/mat/Downloads
+        string selectorfilter_; //=.opk
+        string provider_; //=/home/mat/Downloads/games/rg\-350/regba_fast\ \(1\).opk
+        bool consoleapp_; //=false
+
+    public:
+        DesktopFile();
+        DesktopFile(const string & file);
+        ~DesktopFile();
+
+        bool fromFile(const string & file);
+        bool save(const string & path = "");
+        void remove();
+
+        string toString();
+
+        bool isDirty() { return this->isDirty_; }
+    
+        string title() const { return this->title_; }
+        void title(string val) { 
+            if (val != this->title_) {
+                this->title_ = val;
+                this->isDirty_ = true;
+            }
+        }
+        string description() const { return this->description_; }
+        void description(string val) { 
+            if (val != this->description_) {
+                this->description_ = val;
+                this->isDirty_ = true;
+            }
+        }
+        string icon() const { return this->icon_; }
+        void icon(string val) { 
+            if (val != this->icon_) {
+                this->icon_ = val;
+                this->isDirty_ = true;
+            }
+        }
+        string exec() const { return this->exec_; }
+        void exec(string val) { 
+            if (val != this->exec_) {
+                this->exec_ = val;
+                this->isDirty_ = true;
+            }
+        }
+        string params() const { return this->params_; }
+        void params(string val) { 
+            if (val != this->params_) {
+                this->params_ = val;
+                this->isDirty_ = true;
+            }
+        }
+        string selectordir() const { return this->selectordir_; }
+        void selectordir(string val) { 
+            if (val != this->selectordir_) {
+                this->selectordir_ = val;
+                this->isDirty_ = true;
+            }
+        }
+        string selectorfilter() const { return this->selectorfilter_; }
+        void selectorfilter(string val) { 
+            if (val != this->selectorfilter_) {
+                this->selectorfilter_ = val;
+                this->isDirty_ = true;
+            }
+        }
+        string provider() const { return this->provider_; }
+        void provider(string val) { 
+            if (val != this->provider_) {
+                this->provider_ = val;
+                this->isDirty_ = true;
+            }
+        }
+        bool consoleapp() const { return this->consoleapp_; }
+        void consoleapp(bool val) { 
+            if (val != this->consoleapp_) {
+                this->consoleapp_ = val;
+                this->isDirty_ = true;
+            }
+        }
+};
+
+#endif
+
