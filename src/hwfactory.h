59e420e0f4474cbee8f39f5bd82e466bb2604aca
podulator <github-account@podulator.com>
2019-12-12 12:31:48 +0100

    add performance modes to generic hw layer for testing

--- src/hwfactory.h
+++ src/hwfactory.h
@@ -481,8 +481,28 @@ class HwRg350 : IHardware {
 };
 
 class HwGeneric : IHardware {
-     public:
-        HwGeneric() {};
+    private:
+
+
+        std::string performanceModeMap(std::string fromInternal) {
+            std::unordered_map<string, string>::iterator it;
+            for (it = this->performanceModes_.begin();it != this->performanceModes_.end(); it++) {
+                if (fromInternal == it->first) {
+                    return it->second;;
+                }
+            }
+            return "On demand";
+        }
+    
+        std::unordered_map<std::string, std::string> performanceModes_;
+        std::string performanceMode_ = "ondemand";
+        const std::string defaultPerformanceMode = "ondemand";
+
+    public:
+        HwGeneric() {
+            this->performanceModes_.insert({"ondemand", "On demand"});
+            this->performanceModes_.insert({"performance", "Performance"});
+        };
 
         std::string getSystemdateTime() { 
             time_t now = time(0);
@@ -500,9 +520,42 @@ class HwGeneric : IHardware {
             if (val != "NTSC" && val != "PAL") val = "OFF";
         }
 
-        void setPerformanceMode(std::string alias = "") { return; };
-        std::string getPerformanceMode() { return "Default"; };
-        std::vector<std::string>getPerformanceModes() { return vector<string> { "Default"}; }
+        std::string getPerformanceMode() { 
+            TRACE("enter");
+            return this->performanceModeMap(this->performanceMode_);
+        }
+        void setPerformanceMode(std::string alias = "") {
+            TRACE("raw desired : %s", alias.c_str());
+            string mode = this->defaultPerformanceMode;
+            if (!alias.empty()) {
+                std::unordered_map<string, string>::iterator it;
+                for (it = this->performanceModes_.begin();it != this->performanceModes_.end(); it++) {
+                    TRACE("checking '%s' aginst <%s, %s>", alias.c_str(), it->first.c_str(), it->second.c_str());
+                    if (alias == it->second) {
+                        TRACE("matched it as : %s", it->first.c_str());
+                        mode = it->first;
+                        break;
+                    }
+                }
+            }
+            TRACE("internal desired : %s", mode.c_str());
+            if (mode != this->performanceMode_) {
+                TRACE("update needed : current '%s' vs. desired '%s'", this->performanceMode_.c_str(), mode.c_str());
+                this->performanceMode_ = mode;
+            } else {
+                TRACE("nothing to do");
+            }
+            TRACE("exit");
+        }
+        std::vector<std::string> getPerformanceModes() {
+            std::vector<std::string> results;
+            std::unordered_map<std::string, std::string>::iterator it;
+            for (it = this->performanceModes_.begin(); it != this->performanceModes_.end(); it++) {
+                results.push_back(it->second);
+            }
+            return results;
+        }
+        //std::vector<std::string>getPerformanceModes() { return vector<string> { "Default"}; }
         uint32_t setCPUSpeed(uint32_t mhz) { return mhz; };
 
         void ledOn(int flashSpeed = 250) { return; };
